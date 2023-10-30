a0857069c3e11762af5ab2f6ca8b69a72e4fbccb
Marek Purwin <mpurwin@CreateCtrl.com>
2023-10-30 14:40:53 +0100

    Test

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  application:
+  {
+    name: 'angular-pwa',
+    version: 'Angular 16.2.4',
+    bootstrap: 'Bootstrap 5.3.1',
+    fontawesome: 'Font Awesome 6.4.2',
+  }
+};
